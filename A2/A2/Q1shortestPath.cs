2128d74708ac1e82048c9d3db55badf3338abe0e
Mahsa Ghaderan <mahsa.ghaderan@gmail.com>
2022-08-06 15:11:54 +0430

    Upload Assignment 2

---
+++ A2/A2/Q1shortestPath.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A2
+{
+    public class Q1ShortestPath : Processor
+    {
+        public Q1ShortestPath(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long,long[][], long, long, long>)Solve);
+        
+
+        public long Solve(long NodeCount, long[][] edges, long StartNode,  long EndNode)
+        {
+            Node[] myNodes = new Node[NodeCount];
+            MakeGraph(myNodes,edges);
+            Queue<Tuple<long,int>> bfs = new Queue<Tuple<long, int>>();
+            bfs.Enqueue(new Tuple<long, int>(StartNode,0));
+            myNodes[StartNode-1].IsChecked = true;
+            while (bfs.Count!=0)
+            {
+                Tuple<long, int> checking = bfs.Dequeue();
+                foreach (var neighbour in myNodes[checking.Item1-1].Neighbours)
+                {
+                    if (neighbour == EndNode)
+                        return checking.Item2 + 1;
+                    if (!myNodes[neighbour-1].IsChecked)
+                    {
+                        bfs.Enqueue(new Tuple<long, int>(neighbour, checking.Item2 + 1));
+                        myNodes[neighbour-1].IsChecked = true;
+                    }
+
+                }
+            }
+            return -1;
+        }
+
+        public static void MakeGraph(Node[] myNodes,long[][] edges)
+        {
+            for (int i = 0; i < myNodes.Length; i++)
+                myNodes[i] = new Node(i);
+            foreach (var edge in edges)
+            {
+                myNodes[edge[0]-1].Neighbours.Add(edge[1]);
+                myNodes[edge[1]-1].Neighbours.Add(edge[0]);
+            }
+        }
+    }
+
+    public class Node
+    {
+        public long NodeName;
+        public bool IsChecked;
+        public bool sticker;
+        public List<long> Neighbours;
+        public Node(long name)
+        {
+            this.NodeName = name;
+            Neighbours = new List<long>();
+            this.IsChecked = false;
+        }
+    }
+
+}
---
+++ A2/A2/Q2BipartiteGraph.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A2
+{
+    public class Q2BipartiteGraph : Processor
+    {
+        public Q2BipartiteGraph(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[][], long>)Solve);
+
+
+        public long Solve(long NodeCount, long[][] edges)
+        {
+            Node[] myNodes = new Node[NodeCount];
+            Q1ShortestPath.MakeGraph(myNodes, edges);
+            Queue<long> bfs = new Queue<long>();
+            bfs.Enqueue(1);
+            myNodes[0].IsChecked = true;
+            myNodes[0].sticker = true;
+            while (bfs.Count!=0)
+            {
+                long checking = bfs.Dequeue();
+                foreach (var neighbour in myNodes[checking - 1].Neighbours)
+                {
+                    if (!myNodes[neighbour - 1].IsChecked)
+                    {
+                        bfs.Enqueue(neighbour);
+                        myNodes[neighbour - 1].IsChecked = true;
+                        myNodes[neighbour - 1].sticker = !myNodes[checking-1].sticker;
+                    }
+                    else if (myNodes[checking - 1].sticker == myNodes[neighbour - 1].sticker)
+                        return 0;
+
+                }
+            }
+            return 1;
+        }
+    }
+
+}
